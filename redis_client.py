d63fb6f71142eb7b23675b122e563ac433824ee2
VovaTka4 <EpikBulldogg@mail.ru>
2025-12-12 20:24:03 +0300

    redis client builds from .env key

--- redis_client.py
+++ redis_client.py
@@ -2,13 +2,7 @@ import redis
 from redis import Redis
 import os
 
-#redis_client = redis.from_url(os.getenv("REDIS_URL")) #ЕСЛИ БУДЕТ, СЮДА НУЖНА ССЫЛКА ИЗ Render!
-
-redis_client = Redis(
-    host="localhost",
-    port=6379,
-    decode_responses=True
-)
+redis_client = redis.from_url(os.getenv("REDIS_URL"), decode_responses=True)
 
 try:
     redis_client.ping()
